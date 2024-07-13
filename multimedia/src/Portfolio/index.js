f342cf4c7e374da5eb545a4b8dc570a2591285f3
Teresiawangui <wanguiiteresiaa@gmail.com>
2024-07-14 00:08:27 +0300

    made changes

---
+++ multimedia/src/Portfolio/index.js
@@ -0,0 +1,160 @@
+// import Gitahi from '../Gitahi';
+
+
+
+// function PortFolio(){
+//     return(
+//         <div className='portfolios'>
+//     <Gitahi/>
+   
+//         </div>
+
+//     )
+    
+
+// }
+// export default PortFolio;
+import "./index.css"
+
+
+function Gitahi(){
+    return(
+        <div className="info-2">
+        <div className="info">
+           <div className="details">
+            <div className="title">
+            <div className="introduction"><img src="/Images/Maggy.jpeg" className="pictures" alt="Gitahi"/>
+            <div className="education">
+                <h2>Gitahi</h2>
+                
+           
+                
+                <div className="contact">
+                <h2><b>Contacts</b></h2>
+                    <p id="contact"><b>email:</b>     moureengitahi@gmail.com
+             <br></br>
+             <b>LinkedIn:</b>    Moureen Gitahi
+             <br></br>
+             <b>GitHub:</b>      Moureen Gitahi
+             <br></br>
+             
+             </p>
+             </div>
+             
+             <div className="Akirachix">
+             <h2>Education</h2>
+             <p >AkiraChix</p></div>
+             </div>
+             </div>
+
+            
+        <div id="skills">
+             <div className="personal-statement">
+            <h2><b>Personal Statement</b></h2>
+            
+            <p>The health sector has seen vast technological advancements,
+                 enabling users to access personalized healthcare and real
+                 -time health monitoring through wearable devices and telemedicine.
+                  {/* Despite these advancements, there remains a significant gap
+                   in ensuring seamless integration and interoperability 
+                   among different health monitoring systems and electronic 
+                   health records. As a software developer, Teresia envisions */}
+                    a future where cutting-edge technologies enhance the integration
+                     and accessibility of health data, providing healthcare 
+                     professionals with comprehensive, actionable insights
+                      to provide more personalized care to patients. .</p></div>
+                <div className="skills">
+
+<h2>Skills</h2>
+
+
+<ul id="skills">
+
+<li>Skilled in HTML, CSS, and JavaScript for building dynamic web applications.</li>
+<li> Extensive experience with modern JavaScript frameworks and libraries (e.g., React).</li>
+<li>Proficiency in responsive web design and cross-browser compatibility.</li>
+<li>Proficiency in using design tools such as Figma, Sketch, and Adobe Creative Suite.</li>
+{/* <li>Ability to design and implement robust, scalable, and maintainable software systems.</li>
+<li>Proficiency in software development lifecycle methodologies (e.g., Agile)</li>
+<li>Proficiency in user research methods (e.g., interviews, usability testing, surveys)</li>
+<li>Experience conducting user research to gather insights and inform design decisions</li>
+<li>Ability to analyze user data, identify pain points, and generate actionable recommendations</li> */}
+ </ul>
+
+ </div>
+ </div>
+
+            </div>
+</div>
+<div className="details">
+            <div className="title">
+            <div className="introduction"><img src="/Images/Tuukuo.jpeg" className="pictures" alt="Gitahi"/>
+            <div className="education">
+                <h2>Gitahi</h2>
+                
+           
+                
+                <div className="contact">
+                <h2><b>Contacts</b></h2>
+                    <p id="contact"><b>email:</b>     moureengitahi@gmail.com
+             <br></br>
+             <b>LinkedIn:</b>    Moureen Gitahi
+             <br></br>
+             <b>GitHub:</b>      Moureen Gitahi
+             <br></br>
+             
+             </p>
+             </div>
+             
+             <div className="Akirachix">
+             <h2>Education</h2>
+             <p >AkiraChix</p></div>
+             </div>
+             </div>
+
+            
+        <div id="skills">
+             <div className="personal-statement">
+            <h2><b>Personal Statement</b></h2>
+            
+            <p>The health sector has seen vast technological advancements,
+                 enabling users to access personalized healthcare and real
+                 -time health monitoring through wearable devices and telemedicine.
+                  {/* Despite these advancements, there remains a significant gap
+                   in ensuring seamless integration and interoperability 
+                   among different health monitoring systems and electronic 
+                   health records. As a software developer, Teresia envisions */}
+                    a future where cutting-edge technologies enhance the integration
+                     and accessibility of health data, providing healthcare 
+                     professionals with comprehensive, actionable insights
+                      to provide more personalized care to patients. .</p></div>
+                <div className="skills">
+
+<h2>Skills</h2>
+
+
+<ul id="skills">
+
+<li>Skilled in HTML, CSS, and JavaScript for building dynamic web applications.</li>
+<li> Extensive experience with modern JavaScript frameworks and libraries (e.g., React).</li>
+<li>Proficiency in responsive web design and cross-browser compatibility.</li>
+<li>Proficiency in using design tools such as Figma, Sketch, and Adobe Creative Suite.</li>
+{/* <li>Ability to design and implement robust, scalable, and maintainable software systems.</li>
+<li>Proficiency in software development lifecycle methodologies (e.g., Agile)</li>
+<li>Proficiency in user research methods (e.g., interviews, usability testing, surveys)</li>
+<li>Experience conducting user research to gather insights and inform design decisions</li>
+<li>Ability to analyze user data, identify pain points, and generate actionable recommendations</li> */}
+ </ul>
+
+ </div>
+ </div>
+
+            </div>
+</div>
+
+        </div>
+        
+        
+</div>
+)}
+export default Gitahi;
