e5b5bd087e65a64f707f173524b0fabefc3c7a61
Teresiawangui <wanguiiteresiaa@gmail.com>
2024-07-14 00:06:27 +0300

    made changes

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Esther from './Members';
+import Gitahi from './Portfolio';
+
+
+function App() {
+  return (
+    <div>
+      
+        <Esther/>
+        <Gitahi/>
+
+     
+    </div>
+  );
+  } 
+export  default App;
