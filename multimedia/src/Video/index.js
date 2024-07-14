369785959df4ea6af86508edba500a5913f8002c
Margaret-Nyambura <margaretmacharianyambura@gmail.com>
2024-07-14 11:37:20 +0300

    created videos

---
+++ multimedia/src/Video/index.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import './index.css'
+
+
+
+const Video = ()=>{
+    return(
+
+    
+        
+                <div className="vid"> 
+                <h1 className="chatgpt">ChatGPT Season 1 </h1>
+                  <div className="vidLine1">
+                <div><iframe src="https://www.loom.com/embed/b47031eddc2147a99aebaf32b006af92?sid=d5b32b52-3f4c-4d29-9f41-a2951c7327bc" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen className="video"></iframe>
+                <p className="text"><b>Episode 1:</b>The development of ChatGPT.</p>
+                
+</div>   
+
+<div><iframe src="https://www.loom.com/embed/c9a0867f680148cb96476650bb0948ff?sid=584fca13-1928-43cb-9466-1bd22077e4a9" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen className="video"></iframe>
+                <p className="text"><b>Episode 2:</b> The architecture outline of ChatGPT.</p>
+               
+</div>
+          
+<div ><iframe src="https://www.loom.com/embed/a456880b8c98406db4da1612a303dcd2?sid=bea2c28f-5b66-4b68-988c-3e7f4a586e5b" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen className="video"></iframe>
+                <p className="text"><b>Episode 3:</b> Features of ChatGPT and<br></br> how the ChatGPT generates revenue.
+
+</p>
+                   
+</div>
+</div>
+<div className="vidLine2">
+<div><iframe src="https://www.loom.com/embed/8299ce6892aa4416886aea8ab1467720?sid=6ad43d45-2767-44c5-8034-0c8dd936cc82" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen className="video"></iframe>
+                <p className="text"><b>Episode 4: </b>Positive impacts and shortcomings <br></br>of ChatGPT and the User experience.
+</p>
+                
+</div>
+
+
+
+
+
+   <div><iframe src="https://www.loom.com/embed/785e76518a494715a08761c75c05fb8f?sid=8a19a939-8a75-479f-9b82-36a43b386aac" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen className="video"></iframe>
+ <p className="text"><b>Episode 5:</b> The use case and impact of ChatGPT.</p>
+               
+</div>
+         
+                </div>
+
+
+                </div>
+
+
+                
+    )
+
+};
+
+export default Video;
